917cefd2636131f3a67dfba0ce17ddf3c203a93e
Tembeon <tembeon@gmail.com>
2021-03-06 18:57:35 +0300

    remove top padding

--- lib/ui/screens/news_screen.dart
+++ lib/ui/screens/news_screen.dart
@@ -79,70 +79,77 @@ class _NewsScreenState extends State<NewsScreen> {
             : RefreshIndicator(
                 key: _refreshKey,
                 child: SafeArea(
+                    top: false,
                     child: ListView.builder(
-                  itemCount: _feed.items.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final item = _feed.items[index];
-                    String imgUrl = item.description.substring(
-                        item.description.indexOf("src=") + 5,
-                        item.description.indexOf('alt=') - 2);
-                    return Card(
-                      clipBehavior: Clip.antiAlias,
-                      child: InkWell(
-                        onTap: () => openFeed(item.link),
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              
-                              child: Image.network(
-                                imgUrl,
-                                frameBuilder: (BuildContext context, Widget child,
-                                        int frame, bool wasSynchronouslyLoaded) =>
-                                    wasSynchronouslyLoaded
-                                        ? child
-                                        : AnimatedOpacity(
-                                            child: child,
-                                            opacity: frame == null ? 0 : 1,
-                                            duration: const Duration(seconds: 2),
-                                            curve: Curves.easeOut,
-                                          ),
-                                loadingBuilder: (context, child, progress) =>
-                                    progress == null
-                                        ? child
-                                        : LinearProgressIndicator(
-                                            valueColor: AlwaysStoppedAnimation<
-                                                    Color>(
-                                                Theme.of(context).accentColor),
-                                          ),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.all(10),
-                              child: Column(
-                                children: <Widget>[
-                                  Text(
-                                    item.title,
-                                    style:
-                                        Theme.of(context).textTheme.headline3,
+                      itemCount: _feed.items.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final item = _feed.items[index];
+                        String imgUrl = item.description.substring(
+                            item.description.indexOf("src=") + 5,
+                            item.description.indexOf('alt=') - 2);
+                        return Card(
+                          clipBehavior: Clip.antiAlias,
+                          child: InkWell(
+                            onTap: () => openFeed(item.link),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  child: Image.network(
+                                    imgUrl,
+                                    frameBuilder: (BuildContext context,
+                                            Widget child,
+                                            int frame,
+                                            bool wasSynchronouslyLoaded) =>
+                                        wasSynchronouslyLoaded
+                                            ? child
+                                            : AnimatedOpacity(
+                                                child: child,
+                                                opacity: frame == null ? 0 : 1,
+                                                duration:
+                                                    const Duration(seconds: 2),
+                                                curve: Curves.easeOut,
+                                              ),
+                                    loadingBuilder:
+                                        (context, child, progress) =>
+                                            progress == null
+                                                ? child
+                                                : LinearProgressIndicator(
+                                                    valueColor:
+                                                        AlwaysStoppedAnimation<
+                                                                Color>(
+                                                            Theme.of(context)
+                                                                .accentColor),
+                                                  ),
                                   ),
-                                  Padding(
-                                    padding: EdgeInsets.only(top: 6),
-                                    child: Row(
-                                      children: <Widget>[
-                                        Spacer(),
-                                        Text(item.pubDate),
-                                      ],
-                                    ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.all(10),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Text(
+                                        item.title,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline3,
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.only(top: 6),
+                                        child: Row(
+                                          children: <Widget>[
+                                            Spacer(),
+                                            Text(item.pubDate),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                )),
+                          ),
+                        );
+                      },
+                    )),
                 onRefresh: () => load(),
               ));
   }
