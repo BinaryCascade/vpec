ff56a06d20e841e9443912cbad239ba4745d43dd
Lazurit <lazurit.vladislav@gmail.com>
2021-03-09 00:05:37 +0300

    changed some padding code

--- lib/ui/screens/news_screen.dart
+++ lib/ui/screens/news_screen.dart
@@ -77,88 +77,74 @@ class _NewsScreenState extends State<NewsScreen> {
                 child: SafeArea(
                     top: false,
                     child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          SizedBox(height: 7),
-                          ListView.builder(
-
-                            //this is the solution
-                            physics: ClampingScrollPhysics(),
-                            shrinkWrap: true,
-                            //i want to scream rly hard
-
-                            itemCount: _feed.items.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              final item = _feed.items[index];
-                              String imgUrl = item.description.substring(
-                                  item.description.indexOf("src=") + 5,
-                                  item.description.indexOf('alt=') - 2);
-                              return Padding(
-                                padding: const EdgeInsets.only(left:7, right: 7, bottom: 6),
-                                child: Card(
-                                  clipBehavior: Clip.antiAlias,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: InkWell(
-                                    onTap: () => openFeed(item.link),
-                                    child: Column(
-                                      children: <Widget>[
-                                        Container(
-                                          clipBehavior: Clip.antiAlias,
-                                          decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.all(
-                                                Radius.circular(10)),
-                                          ),
-                                          child: CachedNetworkImage(
-                                            imageUrl: imgUrl,
-                                            fit: BoxFit.fill,
-                                            placeholder: (context, url) =>
-                                                Container(
-                                                    height: 200,
-                                                    child: Image.memory(
-                                                        kTransparentImage)),
-                                          ),
-                                        ),
-                                        Padding(
-                                          padding: EdgeInsets.only(
-                                              left: 13,
-                                              right: 13,
-                                              top: 10,
-                                              bottom: 11),
-                                          child: Column(
-                                            children: <Widget>[
-                                              Text(
-                                                item.title,
-                                                style: Theme.of(context)
-                                                    .textTheme
-                                                    .headline3,
-                                              ),
-                                              Padding(
-                                                padding:
-                                                    EdgeInsets.only(top: 8),
-                                                child: Row(
-                                                  children: <Widget>[
-                                                    Spacer(),
-                                                    Text(DateFormat(
-                                                            'd MMM yyyy, HH:mm')
-                                                        .format(
-                                                            item.pubDate)),
-                                                  ],
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ],
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 6.5, vertical: 5.5),
+                      child: ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: _feed.items.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final item = _feed.items[index];
+                          String imgUrl = item.description.substring(
+                              item.description.indexOf("src=") + 5,
+                              item.description.indexOf('alt=') - 2);
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 2),
+                            child: Card(
+                              clipBehavior: Clip.antiAlias,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: InkWell(
+                                onTap: () => openFeed(item.link),
+                                child: Column(
+                                  children: <Widget>[
+                                    Container(
+                                      clipBehavior: Clip.antiAlias,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(10)),
+                                      ),
+                                      child: CachedNetworkImage(
+                                        imageUrl: imgUrl,
+                                        fit: BoxFit.fill,
+                                        placeholder: (context, url) =>
+                                            Container(
+                                                height: 200,
+                                                child: Image.memory(
+                                                    kTransparentImage)),
+                                      ),
                                     ),
-                                  ),
+                                    Padding(
+                                      padding: EdgeInsets.all(10),
+                                      child: Column(
+                                        children: <Widget>[
+                                          Text(
+                                            item.title,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .headline3,
+                                          ),
+                                          Padding(
+                                            padding: EdgeInsets.only(top: 6),
+                                            child: Row(
+                                              children: <Widget>[
+                                                Spacer(),
+                                                Text(DateFormat(
+                                                        'd MMM yyyy, HH:mm')
+                                                    .format(item.pubDate)),
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              );
-                            },
-                          ),
-                          SizedBox(height: 3)
-                        ],
+                              ),
+                            ),
+                          );
+                        },
                       ),
                     )),
                 onRefresh: () => load(),
