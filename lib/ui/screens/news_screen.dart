486c780268596c9a6fee5fa234b5163764438577
Tembeon <tembeon@gmail.com>
2021-03-13 16:44:56 +0300

    remove RefreshIndicator

--- lib/ui/screens/news_screen.dart
+++ lib/ui/screens/news_screen.dart
@@ -16,9 +16,8 @@ class NewsScreen extends StatefulWidget {
 class _NewsScreenState extends State<NewsScreen> {
   static const String FEED_URL = 'http://energocollege.ru/rss.xml';
   RssFeed _feed;
-  GlobalKey<RefreshIndicatorState> _refreshKey;
 
-  updateFeed(feed) {
+  void updateFeed(feed) {
     setState(() {
       _feed = feed;
     });
@@ -31,11 +30,10 @@ class _NewsScreenState extends State<NewsScreen> {
         forceSafariVC: true,
         forceWebView: false,
       );
-      return null;
     }
   }
 
-  load() async {
+  Future<void> load() async {
     loadFeed().then((result) {
       if (null == result || result.toString().isEmpty) {
         return RssFeed();
@@ -57,7 +55,6 @@ class _NewsScreenState extends State<NewsScreen> {
   @override
   void initState() {
     super.initState();
-    _refreshKey = GlobalKey<RefreshIndicatorState>();
     load();
   }
 
@@ -72,84 +69,80 @@ class _NewsScreenState extends State<NewsScreen> {
             ? Center(
                 child: CircularProgressIndicator(),
               )
-            : RefreshIndicator(
-                key: _refreshKey,
-                child: SafeArea(
-                    top: false,
-                    child: SingleChildScrollView(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 6.5, vertical: 5.5),
-                      child: ListView.builder(
-                        physics: NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: _feed.items.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          final item = _feed.items[index];
-                          String imgUrl = item.description.substring(
-                              item.description.indexOf("src=") + 5,
-                              item.description.indexOf('alt=') - 2);
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 2),
-                            child: Card(
-                              clipBehavior: Clip.antiAlias,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: InkWell(
-                                onTap: () => openFeed(item.link),
-                                child: Column(
-                                  children: <Widget>[
-                                    Container(
-                                      clipBehavior: Clip.antiAlias,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(10)),
-                                      ),
-                                      child: CachedNetworkImage(
-                                        imageUrl: imgUrl,
-                                        fit: BoxFit.fill,
-                                        placeholder: (context, url) =>
-                                            Container(
-                                                height: 200,
-                                                child: Image.memory(
-                                                    kTransparentImage)),
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          horizontal: 15, vertical: 13),
-                                      child: Column(
-                                        children: <Widget>[
-                                          Text(
-                                            item.title,
-                                            style: Theme.of(context).textTheme.headline3,
-                                          ),
-                                          Padding(
-                                            padding: EdgeInsets.only(top: 6),
-                                            child: Row(
-                                              children: <Widget>[
-                                                Spacer(),
-                                                Text(DateFormat(
-                                                        'd MMMM yyyy, HH:mm')
-                                                    .format(item.pubDate),
-                                                    style: Theme.of(context)
-                                                    .textTheme
-                                                    .subtitle1,
-                                                )],
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ],
+            : SafeArea(
+                top: false,
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.symmetric(horizontal: 6.5, vertical: 5.5),
+                  child: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: _feed.items.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final item = _feed.items[index];
+                      String imgUrl = item.description.substring(
+                          item.description.indexOf("src=") + 5,
+                          item.description.indexOf('alt=') - 2);
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 2),
+                        child: Card(
+                          clipBehavior: Clip.antiAlias,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: InkWell(
+                            onTap: () => openFeed(item.link),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  clipBehavior: Clip.antiAlias,
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10)),
+                                  ),
+                                  child: CachedNetworkImage(
+                                    imageUrl: imgUrl,
+                                    fit: BoxFit.fill,
+                                    placeholder: (context, url) => Container(
+                                        height: 200,
+                                        child: Image.memory(kTransparentImage)),
+                                  ),
                                 ),
-                              ),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 15, vertical: 13),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Text(
+                                        item.title,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline3,
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.only(top: 6),
+                                        child: Row(
+                                          children: <Widget>[
+                                            Spacer(),
+                                            Text(
+                                              DateFormat('d MMMM yyyy, HH:mm')
+                                                  .format(item.pubDate),
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .subtitle1,
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
                             ),
-                          );
-                        },
-                      ),
-                    )),
-                onRefresh: () => load(),
-              ));
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                )));
   }
 }
