89fac55adea4224f82a5146d418fce6cbfb4630f
Tembeon <tembeon@gmail.com>
2021-03-14 20:01:54 +0300

    migrate settings_ui to new widgets

--- lib/ui/screens/settings/settings_ui.dart
+++ lib/ui/screens/settings/settings_ui.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:vpec/ui/widgets/styled_widgets.dart';
 import 'package:vpec/utils/icons.dart';
 import 'settings_logic.dart';
@@ -9,46 +7,28 @@ import 'settings_logic.dart';
 Widget buildAccountBlock(BuildContext context) {
   return Column(
     children: [
-      ListTile(
+      styledListTile(
+        context: context,
         onTap: () => SettingsLogic().accountLogin(context),
-        leading: Container(
-          height: double.infinity,
-          child: Icon(
-            Icons.account_circle_outlined,
-            color: Theme.of(context).accentColor,
-            size: 32,
-          ),
-        ),
-        title: Text(
-          'Аккаунт',
-          style: Theme.of(context).textTheme.headline3,
-        ),
-        subtitle: Text(
-          SettingsLogic().getAccountEmail(),
-          style: Theme.of(context).textTheme.subtitle1,
-        ),
-      ),
-      ListTile(
-        onTap: () => SettingsLogic().changeName(context),
-        leading: Icon(
-          Icons.edit_outlined,
+        icon: Icon(
+          Icons.account_circle_outlined,
           color: Theme.of(context).accentColor,
           size: 32,
         ),
-        title: Text(
-          'Имя',
-          style: Theme.of(context).textTheme.headline3,
-        ),
-        subtitle: ValueListenableBuilder(
-          valueListenable: Hive.box('settings').listenable(keys: ['username']),
-          builder: (context, box, child) {
-            return Text(
-              box.get('username', defaultValue: 'Изменить имя'),
-              style: Theme.of(context).textTheme.subtitle1,
-            );
-          },
-        ),
+        title: 'Аккаунт',
+        subtitle: SettingsLogic().getAccountEmail(),
       ),
+      hivedListTile(
+          context: context,
+          onTap: () => SettingsLogic().changeName(context),
+          icon: Icon(
+            Icons.edit_outlined,
+            color: Theme.of(context).accentColor,
+            size: 32,
+          ),
+          title: 'Имя',
+          subtitleKey: 'username',
+          defaultValue: 'Нажмите, чтобы изменить имя'),
     ],
   );
 }
