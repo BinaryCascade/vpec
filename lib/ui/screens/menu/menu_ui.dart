d846decd9ad11d483fafa119e6086c8c0091892a
Tembeon <tembeon@gmail.com>
2021-04-10 20:39:42 +0300

    change Widget to StatelessWidget

--- lib/ui/screens/menu/menu_ui.dart
+++ lib/ui/screens/menu/menu_ui.dart
@@ -3,30 +3,30 @@ import 'package:flutter/material.dart';
 import '../../../models/document_model.dart';
 import '../../../ui/widgets/document_card.dart';
 
-Widget buildDocuments(BuildContext context) {
-  Stream<QuerySnapshot> stream =
-      FirebaseFirestore.instance.collection('documents').snapshots();
-  return Flexible(
-    child: StreamBuilder(
-      stream: stream,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (!snapshot.hasData)
-          return Center(
-            child: CircularProgressIndicator(),
+class ViewDocuments extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Stream<QuerySnapshot> stream =
+        FirebaseFirestore.instance.collection('documents').snapshots();
+    return Flexible(
+      child: StreamBuilder(
+        stream: stream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          return ListView(
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            children: snapshot.data.docs.map((document) {
+              return DocumentCard(
+                document: DocumentModel.fromMap(document.data(), document.id),
+              );
+            }).toList(),
           );
-        return ListView(
-          physics: NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          children: snapshot.data.docs
-              .map((document) {
-                return DocumentCard(
-                    document:
-                        DocumentModel.fromMap(document.data(), document.id),
-                );
-              })
-              .toList(),
-        );
-      },
-    ),
-  );
+        },
+      ),
+    );
+  }
 }
