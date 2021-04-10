09c4af50ae19a337f21dda7f7963c3cc842061e3
Tembeon <tembeon@gmail.com>
2021-04-10 20:42:38 +0300

    change Widget to StatelessWidget

--- lib/ui/screens/announcements/announcements_ui.dart
+++ lib/ui/screens/announcements/announcements_ui.dart
@@ -5,57 +5,61 @@ import 'package:flutter/material.dart';
 import '../../../models/announcement_model.dart';
 import '../../../ui/widgets/announcement_card.dart';
 
-// ListView with data from Firestore
-Widget buildAnnouncementsList(String streamName) {
-  ScrollController _semicircleController = ScrollController();
-  Stream<QuerySnapshot> stream =
-      FirebaseFirestore.instance.collection(streamName).snapshots();
+/// ListView with data from Firestore
+class AnnouncementsList extends StatelessWidget {
+  final String collectionPath;
+  const AnnouncementsList({Key key, this.collectionPath}) : super(key: key);
 
-  return Column(
-    children: <Widget>[
-      Expanded(
-        child: StreamBuilder(
-          stream: stream,
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasError)
-              return Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Text(
-                    'Произошла ошибка при получении данных, пожалуйста, '
-                    'войдите в аккаунт',
-                    style: Theme.of(context).textTheme.bodyText1,
-                    textAlign: TextAlign.center,
+  @override
+  Widget build(BuildContext context) {
+    ScrollController _semicircleController = ScrollController();
+    Stream<QuerySnapshot> stream =
+        FirebaseFirestore.instance.collection(collectionPath).snapshots();
+
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: StreamBuilder(
+            stream: stream,
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError)
+                return Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Text(
+                      'Произошла ошибка при получении данных, пожалуйста, '
+                      'войдите в аккаунт',
+                      style: Theme.of(context).textTheme.bodyText1,
+                      textAlign: TextAlign.center,
+                    ),
                   ),
-                ),
-              );
-            if (!snapshot.hasData)
-              return Center(
-                child: CircularProgressIndicator(),
-              );
+                );
+              if (!snapshot.hasData)
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
 
-            return DraggableScrollbar.semicircle(
-              backgroundColor: Theme.of(context).primaryColor,
-              child: ListView(
+              return DraggableScrollbar.semicircle(
+                backgroundColor: Theme.of(context).primaryColor,
+                child: ListView(
+                  controller: _semicircleController,
+                  children: snapshot.data.docs
+                      .map((document) {
+                        return AnnouncementCard(
+                            announcement: AnnouncementModel.fromMap(
+                                document.data(), document.id));
+                      })
+                      .toList()
+                      .reversed
+                      .toList(),
+                ),
                 controller: _semicircleController,
-                children: snapshot.data.docs
-                    .map((document) {
-                      return AnnouncementCard(
-                          announcement: AnnouncementModel.fromMap(
-                              document.data(), document.id));
-                    })
-                    .toList()
-                    .reversed
-                    .toList(),
-              ),
-              controller: _semicircleController,
-              labelConstraints:
-                  BoxConstraints.tightFor(width: 80.0, height: 30.0),
-            );
-          },
+              );
+            },
+          ),
         ),
-      ),
-    ],
-  );
+      ],
+    );
+  }
 }
