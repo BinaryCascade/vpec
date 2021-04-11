74fc8ed65e1c7ae5313c9bd434779feef717f41e
Tembeon <tembeon@gmail.com>
2021-04-11 18:59:22 +0300

    wrap CabinetsMapScreen with ChangeNotifierProvider

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:vpec/ui/screens/documents_screen.dart';
+import 'package:provider/provider.dart';
 
+import '../ui/screens/cabinets_map/cabinets_map_logic.dart';
 import '../splash.dart';
 import '../ui/screens/about_app/about_app_screen.dart';
 import '../ui/screens/admin_screen.dart';
 import '../ui/screens/bottom_bar_screen.dart';
 import '../ui/screens/cabinets_map/cabinets_map_screen.dart';
+import '../ui/screens/documents_screen.dart';
 import '../ui/screens/job_quiz/job_quiz_screen.dart';
 import '../ui/screens/settings/settings_screen.dart';
 import '../ui/screens/teacher_screen.dart';
@@ -17,11 +19,12 @@ class Routes {
     '/home': (context) => BottomBarScreen(),
     '/settings': (context) => SettingsScreen(),
     '/view_document': (context) => DocumentViewScreen(),
-    '/cabinets': (context) => CabinetsMapScreen(),
+    '/cabinets': (context) => ChangeNotifierProvider(
+        create: (_) => CabinetsMapLogic(), child: CabinetsMapScreen()),
     '/administration': (context) => AdminScreen(),
     '/teacher': (context) => TeacherScreen(),
     '/about': (context) => AboutAppScreen(),
     '/job_quiz': (context) => JobQuizScreen(),
-    '/documents' : (context) => DocumentsScreen(),
+    '/documents': (context) => DocumentsScreen(),
   };
 }
