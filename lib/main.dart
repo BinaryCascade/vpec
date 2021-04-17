c228b0c790efcbfa88ab26dc2db1211162f92ae3
Tembeon <tembeon@gmail.com>
2021-04-17 11:13:13 +0300

    migrate to new function initHive

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 import 'ui/theme.dart';
+import 'utils/hive_helper.dart';
 import 'utils/routes.dart';
 import 'utils/theme_helper.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-  await Hive.openBox('settings');
+  await HiveHelper().initHive();
   await Firebase.initializeApp().whenComplete(() => runApp(VPECApp()));
 }
-
 
 class VPECApp extends StatefulWidget {
   @override
