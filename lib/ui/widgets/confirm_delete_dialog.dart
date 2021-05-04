4dba1dc5989267dd8d199eb8ef5365a182ad6d63
Tembeon <tembeon@gmail.com>
2021-05-04 15:06:10 +0300

    make ConfirmDeleteDialogUI widget

---
+++ lib/ui/widgets/confirm_delete_dialog.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class DeleteDialogUI extends StatelessWidget {
+  final void Function() onDelete;
+
+  const DeleteDialogUI({Key? key, required this.onDelete}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 10),
+          child: SizedBox(
+            width: double.infinity,
+            child: OutlinedButton(
+              style: Theme.of(context).outlinedButtonTheme.style,
+              onPressed: () => Navigator.pop(context),
+              child: Text(
+                'Отмена',
+                style: TextStyle(
+                    color: Theme.of(context).textTheme.bodyText1!.color),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: OutlinedButton(
+            style: Theme.of(context).outlinedButtonTheme.style,
+            onPressed: () {
+              Navigator.pop(context);
+              onDelete();
+            },
+            child: Text(
+              'Удалить',
+              style: TextStyle(
+                  color: Theme.of(context).textTheme.bodyText1!.color),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
