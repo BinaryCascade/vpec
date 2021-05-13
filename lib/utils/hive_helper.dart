e9336a6772b0aa1f58664f8d565885c17abdd0ed
Tembeon <tembeon@gmail.com>
2021-05-13 20:45:59 +0300

    make functions static

--- lib/utils/hive_helper.dart
+++ lib/utils/hive_helper.dart
@@ -4,17 +4,17 @@ import 'package:hive_flutter/hive_flutter.dart';
 class HiveHelper {
 
 
-  dynamic getValue(String key) {
+  static dynamic getValue(String key) {
     Box settings = Hive.box('settings');
     return settings.get(key);
   }
 
-  void saveValue({String? key, dynamic value}) {
+  static void saveValue({String? key, dynamic value}) {
     Box settings = Hive.box('settings');
     settings.put(key, value);
   }
 
-  void removeValue(String key) {
+  static void removeValue(String key) {
     Box settings = Hive.box('settings');
     settings.delete(key);
   }
