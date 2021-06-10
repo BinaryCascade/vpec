8ac1ce221b6b9f1e2345afce295cd305f1a5d8a4
Tembeon <tembeon@gmail.com>
2021-06-10 14:53:20 +0300

    add more func to get info about account

--- lib/ui/screens/settings/settings_logic.dart
+++ lib/ui/screens/settings/settings_logic.dart
@@ -12,11 +12,11 @@ import '../../../utils/theme_helper.dart';
 import 'settings_ui.dart';
 
 enum UserMode {
-  admin,
-  student,
-  employee,
-  teacher,
-  enrollee,
+  admin, // have access to view all announcements and edit mode
+  student, // can see only public announcements
+  employee, // can see public and employee announcements
+  teacher, // can see public and teachers announcements
+  enrollee, // can't see anything except info about college
 }
 
 class SettingsLogic extends ChangeNotifier {
@@ -25,7 +25,7 @@ class SettingsLogic extends ChangeNotifier {
   late StreamSubscription<User?> authListener;
 
   static bool get checkIsInEditMode {
-    return SettingsLogic().getAccountEmail()!.isNotEmpty &&
+    return SettingsLogic.getAccountMode() == UserMode.admin &&
         (HiveHelper.getValue('isEditMode') ?? false);
   }
 
@@ -95,19 +95,84 @@ class SettingsLogic extends ChangeNotifier {
   static UserMode getAccountMode() {
     FirebaseAuth auth = FirebaseAuth.instance;
 
-    switch (auth.currentUser!.email) {
-      case 'admin@energocollege.ru':
-        return UserMode.admin;
-      case 'employee@energocollege.ru':
-        return UserMode.employee;
-      case 'teacher@energocollege.ru':
-        return UserMode.teacher;
-      default:
-        if (HiveHelper.getValue('isEnrollee') == true) {
-          return UserMode.enrollee;
-        } else {
+    if (auth.currentUser != null) {
+      switch (auth.currentUser!.email) {
+        case 'admin@energocollege.ru':
+          return UserMode.admin;
+        case 'employee@energocollege.ru':
+          return UserMode.employee;
+        case 'teacher@energocollege.ru':
+          return UserMode.teacher;
+        case 'student@energocollege.ru':
           return UserMode.student;
+        default:
+          return UserMode.enrollee;
+      }
+    } else {
+      return UserMode.enrollee;
+    }
+  }
+
+  static String getAccountModeText() {
+    switch (getAccountMode()) {
+      case UserMode.admin:
+        return 'Администратор';
+      case UserMode.student:
+        return 'Студент';
+      case UserMode.employee:
+        return 'Работник';
+      case UserMode.teacher:
+        return 'Преподаватель';
+      case UserMode.enrollee:
+        return 'Абитуриент';
+    }
+  }
+
+  static bool doAccountHaveAccess(UserMode requiredMode) {
+    switch (getAccountMode()) {
+      case UserMode.admin:
+        return true; // admin have access to anything
+      case UserMode.student:
+        if (requiredMode == UserMode.enrollee ||
+            requiredMode == UserMode.student) {
+          return true; // students can see stuffs for enrolle and students
+        } else {
+          return false;
         }
+      case UserMode.employee:
+        if (requiredMode == UserMode.employee ||
+            requiredMode == UserMode.student ||
+            requiredMode == UserMode.enrollee) {
+          return true;
+          // employee can see stuffs only for employee, not for teachers or admin
+        } else {
+          return false;
+        }
+      case UserMode.teacher:
+        if (requiredMode == UserMode.teacher ||
+            requiredMode == UserMode.student ||
+            requiredMode == UserMode.enrollee) {
+          return true;
+          // teachers can see stuffs only for teachers, not for employee or admin
+        } else {
+          return false;
+        }
+      case UserMode.enrollee:
+        if (requiredMode == UserMode.enrollee) {
+          return true;
+          // well... enrolle can see something only for enrolle
+        } else {
+          return false;
+        }
+    }
+  }
+
+  static bool isAccountModeLowLevel() {
+    if (getAccountMode() == UserMode.student ||
+        getAccountMode() == UserMode.enrollee) {
+      return true;
+    } else {
+      return false;
     }
   }
 
@@ -138,8 +203,8 @@ class SettingsLogic extends ChangeNotifier {
               Navigator.pop(context);
             },
             alwaysLightThemeDocumentChanged: (value) {
-              HiveHelper
-                  .saveValue(key: 'alwaysLightThemeDocument', value: value);
+              HiveHelper.saveValue(
+                  key: 'alwaysLightThemeDocument', value: value);
             },
           );
         },
