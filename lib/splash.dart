edf70cbebb68daae3666490e41598fc90e647605
Tembeon <tembeon@gmail.com>
2021-07-05 14:35:41 +0300

    add check for first launch

--- lib/splash.dart
+++ lib/splash.dart
@@ -4,6 +4,8 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:quick_actions/quick_actions.dart';
+import 'package:vpec/ui/screens/login/login_screen.dart';
+import 'package:vpec/ui/screens/settings/settings_logic.dart';
 
 import 'ui/screens/bottom_bar/bottom_bar_logic.dart';
 import 'utils/hive_helper.dart';
@@ -18,6 +20,8 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  bool userCompleteLoginPage = !(HiveHelper.getValue('isUserEnrolle') == null);
+
   @override
   void initState() {
     _initApp();
@@ -33,54 +37,59 @@ class _SplashScreenState extends State<SplashScreen> {
     await initializeDateFormatting('ru');
     Intl.defaultLocale = 'ru';
 
-    // if app running on Android or iOS, make QuickActions
-    if (defaultTargetPlatform == TargetPlatform.android ||
-        defaultTargetPlatform == TargetPlatform.iOS) {
-      final QuickActions quickActions = QuickActions();
-      quickActions.initialize((shortcutType) {
-        switch (shortcutType) {
-          case 'action_news':
-            context.read<BottomBarLogic>().setIndex(0);
-            break;
-          case 'action_alerts':
-            context.read<BottomBarLogic>().setIndex(1);
-            break;
-          case 'action_timetable':
-            context.read<BottomBarLogic>().setIndex(2);
-            break;
-          case 'action_schedule':
-            context.read<BottomBarLogic>().setIndex(3);
-            break;
-        }
-      });
+    if (userCompleteLoginPage) {
+      // if app running on Android or iOS, make QuickActions
+      if (defaultTargetPlatform == TargetPlatform.android ||
+          defaultTargetPlatform == TargetPlatform.iOS) {
+        final QuickActions quickActions = QuickActions();
+        quickActions.initialize((shortcutType) {
+          switch (shortcutType) {
+            case 'action_news':
+              context.read<BottomBarLogic>().setIndex(0);
+              break;
+            case 'action_alerts':
+              context.read<BottomBarLogic>().setIndex(1);
+              break;
+            case 'action_timetable':
+              context.read<BottomBarLogic>().setIndex(2);
+              break;
+            case 'action_schedule':
+              context.read<BottomBarLogic>().setIndex(3);
+              break;
+          }
+        });
 
-      quickActions.setShortcutItems(<ShortcutItem>[
-        const ShortcutItem(
-            type: 'action_news', localizedTitle: 'События', icon: 'ic_news'),
-        const ShortcutItem(
-            type: 'action_alerts',
-            localizedTitle: 'Объявления',
-            icon: 'ic_alerts'),
-        const ShortcutItem(
-            type: 'action_timetable',
-            localizedTitle: 'Звонки',
-            icon: 'ic_timetable'),
-        const ShortcutItem(
-            type: 'action_schedule',
-            localizedTitle: 'Расписание',
-            icon: 'ic_schedule')
-      ]);
-    }
-    // open bottom bar index by setting "launch on start"
-    if (HiveHelper.getValue('launchOnStart') != null) {
-      int? givenIndex = HiveHelper.getValue('launchOnStart');
-      context.read<BottomBarLogic>().setIndex(givenIndex);
+        quickActions.setShortcutItems(<ShortcutItem>[
+          const ShortcutItem(
+              type: 'action_news', localizedTitle: 'События', icon: 'ic_news'),
+          const ShortcutItem(
+              type: 'action_alerts',
+              localizedTitle: 'Объявления',
+              icon: 'ic_alerts'),
+          const ShortcutItem(
+              type: 'action_timetable',
+              localizedTitle: 'Звонки',
+              icon: 'ic_timetable'),
+          const ShortcutItem(
+              type: 'action_schedule',
+              localizedTitle: 'Расписание',
+              icon: 'ic_schedule')
+        ]);
+      }
+      // open bottom bar index by setting "launch on start"
+      if (HiveHelper.getValue('launchOnStart') != null) {
+        int? givenIndex = HiveHelper.getValue('launchOnStart');
+        context.read<BottomBarLogic>().setIndex(givenIndex);
+      }
+    } else {
+      final QuickActions quickActions = QuickActions();
+      quickActions.clearShortcutItems();
     }
   }
 
   @override
   Widget build(BuildContext context) {
     _loadingSettings();
-    return widget.child!;
+    return userCompleteLoginPage ? widget.child! : const LoginScreen();
   }
 }
