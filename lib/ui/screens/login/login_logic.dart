d2aa7fbc786fe1e311b9bacb04bb8a9e80ec8127
Tembeon <tembeon@gmail.com>
2021-07-05 17:05:37 +0300

    move user to app after login

--- lib/ui/screens/login/login_logic.dart
+++ lib/ui/screens/login/login_logic.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:vpec/ui/screens/settings/settings_logic.dart';
+import 'package:vpec/utils/hive_helper.dart';
 
 class LoginLogic extends ChangeNotifier {
+  static Future<void> openLogin(BuildContext context) async {
+    await SettingsLogic.accountLogin(context);
+    switch (SettingsLogic.getAccountMode()) {
+      case UserMode.admin:
+        continueToApp(context);
+        break;
+      case UserMode.student:
+        continueToApp(context);
+        break;
+      case UserMode.employee:
+        continueToApp(context);
+        break;
+      case UserMode.teacher:
+        continueToApp(context);
+        break;
+      case UserMode.enrollee:
+        // enrollee can't login to account
+        break;
+    }
+  }
 
+  static void continueToApp(BuildContext context) {
+    Navigator.popAndPushNamed(context, '/');
+    HiveHelper.saveValue(key: 'isUserEnrollee', value: false);
+  }
 }
--- lib/ui/screens/login/login_screen.dart
+++ lib/ui/screens/login/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vpec/ui/screens/login/login_logic.dart';
 import 'package:vpec/ui/screens/settings/settings_logic.dart';
 import 'package:vpec/utils/buttons.dart';
 import 'package:vpec/utils/theme_helper.dart';
@@ -19,18 +20,16 @@ class LoginScreen extends StatelessWidget {
           children: <Widget>[
             Text(
               'ВЭК — расписание, звонки и события',
-              style: Theme.of(context).textTheme.headline5!.copyWith(
-                fontSize: 24.0
-              ),
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5!
+                  .copyWith(fontSize: 24.0),
             ),
             Padding(
               padding: const EdgeInsets.only(bottom: 8.0, top: 20.0),
               child: StyledOutlinedButton(
                 text: 'Войти в аккаунт',
-                onPressed: () async {
-                  await SettingsLogic.accountLogin(context);
-                  // TODO: check for successfully login
-                },
+                onPressed: () async => await LoginLogic.openLogin(context),
               ),
             ),
             StyledOutlinedButton(
