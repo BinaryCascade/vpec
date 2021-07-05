9aab5ca13d4ab6219d5e47609f213030ed44cde5
Tembeon <tembeon@gmail.com>
2021-07-05 14:34:18 +0300

    add dummy enrolle screen

---
+++ lib/ui/screens/enrolle/enrolle_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EnrolleScreen extends StatelessWidget {
+  const EnrolleScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Enrolle screen'),
+      ),
+    );
+  }
+}
