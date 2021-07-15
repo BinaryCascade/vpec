5117bff189a7396a0f41f6428c744b730eeb8f1a
Tembeon <tembeon@gmail.com>
2021-07-15 15:20:38 +0300

    update DocumentModel

--- lib/models/document_model.dart
+++ lib/models/document_model.dart
@@ -1,16 +1,25 @@
 // this model used in menu_ui
 
 class DocumentModel {
-  final String? title;
-  final String? subtitle;
-  final String? url;
+  final String title;
+  final String subtitle;
+  final String? url; // can be null if doc type is MD
+  final String? data; // can be null if doc type if PDF
+  final String type;
 
-  DocumentModel({this.title, this.subtitle, this.url});
+  DocumentModel(
+      {required this.title,
+      required this.subtitle,
+      this.url,
+      this.data,
+      required this.type});
 
   DocumentModel.fromMap(Map<String, dynamic> data, String id)
       : this(
           title: data['title'],
           subtitle: data['subtitle'],
           url: data['url'],
+          data: data['data'],
+          type: data['type'],
         );
 }
