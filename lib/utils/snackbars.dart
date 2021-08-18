e13d12916f72768390f092140beecf0ce9b089a2
Tembeon <tembeon@gmail.com>
2021-08-18 23:38:17 +0300

    add support for custom Duration

--- lib/utils/snackbars.dart
+++ lib/utils/snackbars.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
-void showSnackBar(BuildContext context,
-    {required String text, SnackBarBehavior behavior = SnackBarBehavior.floating}) {
+void showSnackBar(
+  BuildContext context, {
+  required String text,
+  SnackBarBehavior behavior = SnackBarBehavior.floating,
+  Duration duration = const Duration(milliseconds: 4000),
+}) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
     behavior: behavior,
+    duration: duration,
     content: Text(text),
   ));
 }
