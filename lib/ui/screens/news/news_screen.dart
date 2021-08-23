b9f93315062129cdd87a16626de4c1b85d17890d
Tembeon <tembeon@gmail.com>
2021-08-23 23:56:20 +0300

    move SafeArea down in widget tree

--- lib/ui/screens/news/news_screen.dart
+++ lib/ui/screens/news/news_screen.dart
@@ -25,25 +25,24 @@ class NewsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-            top: false,
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              child: Consumer<NewsLogic>(
-                builder: (context, state, child) {
-                  return FutureBuilder<RssFeed?>(
-                    future: state.loadFeed(),
-                    builder: (BuildContext context,
-                        AsyncSnapshot<RssFeed?> snapshot) {
-                      if (snapshot.hasData) {
-                        return NewsListView(feed: snapshot.data);
-                      }
-                      return const LoadingIndicator();
-                    },
-                  );
-                },
-              ),
-            )));
+    return Scaffold(body: Consumer<NewsLogic>(
+      builder: (context, state, child) {
+        return FutureBuilder<RssFeed?>(
+          future: state.loadFeed(),
+          builder: (BuildContext context, AsyncSnapshot<RssFeed?> snapshot) {
+            if (snapshot.hasData) {
+              return SafeArea(
+                  top: false,
+                  child: SingleChildScrollView(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: NewsListView(feed: snapshot.data),
+                  ));
+            }
+            return const LoadingIndicator();
+          },
+        );
+      },
+    ));
   }
 }
