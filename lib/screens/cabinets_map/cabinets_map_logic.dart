a442fcf44e58f2712f06096a7d19557d4e3d73ac
Tembeon <tembeon@gmail.com>
2022-01-16 17:59:02 +0300

    code refactor

--- lib/screens/cabinets_map/cabinets_map_logic.dart
+++ lib/screens/cabinets_map/cabinets_map_logic.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/src/provider.dart';
+import 'package:provider/provider.dart';
 
 import '/utils/theme_helper.dart';
 
