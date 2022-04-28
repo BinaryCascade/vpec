0793ef0529dcf89e74054712bb97357d0972628b
Tembeon <tembeon@gmail.com>
2022-04-28 20:55:33 +0300

    added navigator key to the MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,8 @@ import 'utils/notifications/local_notifications.dart';
 import 'utils/routes/routes.dart';
 import 'utils/theme_helper.dart';
 
+GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setPathUrlStrategy(); // remove # from url path
@@ -79,6 +81,7 @@ class _VPECAppState extends State<VPECApp> {
       ],
       theme: themeData(),
       darkTheme: darkThemeData(),
+      navigatorKey: navigatorKey,
       themeMode: context.watch<ThemeNotifier>().themeMode,
       initialRoute: '/',
       onGenerateRoute: Routes.router.generator,
