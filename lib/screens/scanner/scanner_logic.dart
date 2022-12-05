a6c4e103adc51e65f78182b2acc5959fbb30ec99
Lazurit <lazurit.vladislav@gmail.com>
2022-12-06 03:31:02 +0600

    WIP commit

--- lib/screens/scanner/scanner_logic.dart
+++ lib/screens/scanner/scanner_logic.dart
@@ -1,14 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-class MyMobileScannerController extends MobileScannerController {
-  Future<bool> get isDenied async {
-    if (isStarting) {
-      MobileScannerState state = MobileScannerState
-          .values[await methodChannel.invokeMethod('state') as int];
+class ScannerStateNotifier extends ValueNotifier<bool> {
+  ScannerStateNotifier(bool value) : super(value) {
+    startListeningToUpdates();
+  }
 
-      return state == MobileScannerState.denied;
-    }
+  final MobileScannerController _controller = MobileScannerController();
 
-    return true;
+  MobileScannerController get getController => _controller;
+
+  MethodChannel get getChannel => _controller.methodChannel;
+
+  void startListeningToUpdates() {
+    Timer.periodic(const Duration(milliseconds: 300), (timer) async {
+      value = await isAuthorized;
+
+      if (value) {
+        notifyListeners();
+        timer.cancel();
+      }
+    });
+  }
+
+  Future<bool> get isAuthorized async {
+    var state = MobileScannerState
+        .values[await getChannel.invokeMethod('state') as int];
+
+    // if (state == MobileScannerState.undetermined) {
+
+    //   print('AADJSDJKSDHKJASHKDJHAKS result');
+    // }
+
+    // print('!!!!!!!!!! $state');
+
+    return state == MobileScannerState.authorized;
   }
 }
--- lib/screens/scanner/scanner_screen.dart
+++ lib/screens/scanner/scanner_screen.dart
@@ -1,14 +1,19 @@
+import 'package:app_settings/app_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
 import '/screens/login/login_logic.dart';
 import 'scanner_logic.dart';
 
-class ScannerScreen extends StatelessWidget {
+class ScannerScreen extends StatefulWidget {
   const ScannerScreen({Key? key}) : super(key: key);
 
-  // formatted as https://example.com/
-  final String domainAddress = 'https://energocollege.web.app/';
+  @override
+  State<ScannerScreen> createState() => _ScannerScreenState();
+}
+
+class _ScannerScreenState extends State<ScannerScreen> {
+  final scannerState = ScannerStateNotifier(false);
 
   @override
   Widget build(BuildContext context) {
@@ -17,50 +22,27 @@ class ScannerScreen extends StatelessWidget {
         title: const Text('Отсканируйте QR для входа'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Flexible(
-            child: FutureBuilder<bool>(
-              future: MyMobileScannerController().isDenied,
-              initialData: true,
-              builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-                if (snapshot.hasData) {
-                  if (!snapshot.data!) {
-                    return MobileScanner(
-                      controller: MyMobileScannerController(),
-                      onDetect: (barcode, _) {
-                        if (barcode.type == BarcodeType.url) {
-                          String url = barcode.url!.url!;
-                          if (url.startsWith(domainAddress)) {
-                            Navigator.pushNamed(
-                              context,
-                              url.replaceFirst(domainAddress, '/'),
-                            );
-                          }
-                        }
-                      },
-                    );
-                  }
-                }
-
-                return const Text('youre duuumb');
+          Expanded(
+            child: ValueListenableBuilder(
+              valueListenable: scannerState,
+              builder: (context, value, child) {
+                // return ScannerWidget();
+                return AnimatedCrossFade(
+                  alignment: Alignment.center,
+                  duration: const Duration(milliseconds: 3000),
+                  firstChild: ScannerWidget(),
+                  secondChild: NoAccessToCamera(),
+                  crossFadeState: value
+                      ? CrossFadeState.showFirst
+                      : CrossFadeState.showSecond,
+                  // child:
+                  //     value ? const ScannerWidget() : const NoAccessToCamera(),
+                );
               },
             ),
-            // child: !MyMobileScannerController().isDenied
-            //     ? MobileScanner(
-            //         controller: MyMobileScannerController(),
-            //         onDetect: (barcode, _) {
-            //           if (barcode.type == BarcodeType.url) {
-            //             String url = barcode.url!.url!;
-            //             if (url.startsWith(domainAddress)) {
-            //               Navigator.pushNamed(
-            //                 context,
-            //                 url.replaceFirst(domainAddress, '/'),
-            //               );
-            //             }
-            //           }
-            //         },
-            //       )
-            //     : const Text('youre dumb'),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 16),
@@ -79,6 +61,65 @@ class ScannerScreen extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class ScannerWidget extends StatelessWidget {
+  const ScannerWidget({
+    super.key,
+  });
+
+  // formatted as https://example.com/
+  static const String domainAddress = 'https://energocollege.web.app/';
+
+  @override
+  Widget build(BuildContext context) {
+    return MobileScanner(
+      onDetect: (barcode, _) {
+        if (barcode.type == BarcodeType.url) {
+          String url = barcode.url!.url!;
+          if (url.startsWith(domainAddress)) {
+            Navigator.pushNamed(
+              context,
+              url.replaceFirst(domainAddress, '/'),
+            );
+          }
+        }
+      },
+    );
+  }
+}
+
+class NoAccessToCamera extends StatelessWidget {
+  const NoAccessToCamera({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.no_photography_outlined,
+          size: 120,
+          color: Theme.of(context).textTheme.subtitle1!.color,
+        ),
+        const SizedBox(height: 20),
+        Text(
+          'Нет доступа к камере',
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.headline4!.copyWith(
+                fontSize: 22,
+              ),
+        ),
+        const SizedBox(height: 12),
+        Center(
+          child: ElevatedButton(
+            child: const Text('Перейти в настройки'),
+            onPressed: () => AppSettings.openAppSettings(asAnotherTask: true),
+          ),
+        ),
+      ],
     );
   }
 }
