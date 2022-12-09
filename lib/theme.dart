29ae13e110dd19885037965d981bbeef58ed4962
Lazurit <lazurit.vladislav@gmail.com>
2022-12-10 01:17:53 +0600

    introduce `ColorPalette` theme extension

--- lib/theme.dart
+++ lib/theme.dart
@@ -14,6 +14,73 @@ const FontWeight bold = FontWeight.w700;
 const FontWeight extraBold = FontWeight.w800;
 const FontWeight black = FontWeight.w900;
 
+@immutable
+class ColorPalette extends ThemeExtension<ColorPalette> {
+  const ColorPalette({
+    required this.accentColor,
+    required this.highEmphasis,
+    required this.mediumEmphasis,
+    required this.lowEmphasis,
+    required this.backgroundSurface,
+    required this.levelOneSurface,
+    required this.levelTwoSurface,
+    required this.levelThreeSurface,
+  });
+
+  final Color accentColor;
+  final Color highEmphasis;
+  final Color mediumEmphasis;
+  final Color lowEmphasis;
+  final Color backgroundSurface;
+  final Color levelOneSurface;
+  final Color levelTwoSurface;
+  final Color levelThreeSurface;
+
+  @override
+  ThemeExtension<ColorPalette> copyWith({
+    Color? accentColor,
+    Color? highEmphasis,
+    Color? mediumEmphasis,
+    Color? lowEmphasis,
+    Color? backgroundSurface,
+    Color? levelOneSurface,
+    Color? levelTwoSurface,
+    Color? levelThreeSurface,
+  }) {
+    return ColorPalette(
+      accentColor: accentColor ?? this.accentColor,
+      highEmphasis: highEmphasis ?? this.highEmphasis,
+      mediumEmphasis: mediumEmphasis ?? this.mediumEmphasis,
+      lowEmphasis: lowEmphasis ?? this.lowEmphasis,
+      backgroundSurface: backgroundSurface ?? this.backgroundSurface,
+      levelOneSurface: levelOneSurface ?? this.levelOneSurface,
+      levelTwoSurface: levelTwoSurface ?? this.levelTwoSurface,
+      levelThreeSurface: levelThreeSurface ?? this.levelThreeSurface,
+    );
+  }
+
+  @override
+  ThemeExtension<ColorPalette> lerp(
+    ThemeExtension<ColorPalette>? other,
+    double t,
+  ) {
+    if (other is! ColorPalette) return this;
+
+    return ColorPalette(
+      accentColor: Color.lerp(accentColor, other.accentColor, t)!,
+      highEmphasis: Color.lerp(highEmphasis, other.highEmphasis, t)!,
+      mediumEmphasis: Color.lerp(mediumEmphasis, other.mediumEmphasis, t)!,
+      lowEmphasis: Color.lerp(lowEmphasis, other.lowEmphasis, t)!,
+      backgroundSurface:
+          Color.lerp(backgroundSurface, other.backgroundSurface, t)!,
+      levelOneSurface: Color.lerp(levelOneSurface, other.levelOneSurface, t)!,
+      levelTwoSurface: Color.lerp(levelTwoSurface, other.levelTwoSurface, t)!,
+      levelThreeSurface:
+          Color.lerp(levelThreeSurface, other.levelThreeSurface, t)!,
+    );
+  }
+}
+
 ThemeData themeData() {
   //light theme
   const accentColor = Color(0xFF133676);
@@ -27,6 +94,18 @@ ThemeData themeData() {
   final levelThreeSurface = Color.alphaBlend(surfaceIncrement, levelTwoSurface);
 
   return ThemeData(
+    extensions: [
+      ColorPalette(
+        accentColor: accentColor,
+        highEmphasis: highEmphasis,
+        mediumEmphasis: mediumEmphasis,
+        lowEmphasis: lowEmphasis,
+        backgroundSurface: backgroundSurface,
+        levelOneSurface: levelOneSurface,
+        levelTwoSurface: levelTwoSurface,
+        levelThreeSurface: levelThreeSurface,
+      ),
+    ],
     navigationBarTheme: NavigationBarThemeData(
       backgroundColor: levelTwoSurface,
       height: 65,
@@ -205,6 +284,18 @@ ThemeData darkThemeData() {
   final levelThreeSurface = Color.alphaBlend(surfaceIncrement, levelTwoSurface);
 
   return ThemeData.dark().copyWith(
+    extensions: [
+      ColorPalette(
+        accentColor: accentColor,
+        highEmphasis: highEmphasis,
+        mediumEmphasis: mediumEmphasis,
+        lowEmphasis: lowEmphasis,
+        backgroundSurface: backgroundSurface,
+        levelOneSurface: levelOneSurface,
+        levelTwoSurface: levelTwoSurface,
+        levelThreeSurface: levelThreeSurface,
+      ),
+    ],
     navigationBarTheme: NavigationBarThemeData(
       backgroundColor: levelTwoSurface,
       height: 65,
